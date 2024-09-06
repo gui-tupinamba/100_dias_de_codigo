f7f9487d0dc5a19f5314351cd7c035e636771199
Guilherme Tupinambá <guilhermebritotmelo@gmail.com>
2024-09-05 23:24:07 -0400

    Dia 02 e Dia 03

---
+++ impar_par.py
@@ -0,0 +1,8 @@
+num = input("Insira um número: ")
+
+if((num % 2) == 0):
+    result = "Par"
+else:
+    result = "Ímpar"
+
+print(f"O número {num} é {result}.")
---
+++ soma.py
@@ -0,0 +1,5 @@
+num1 = float(input('Insira um priemrio valor: '))
+num2 = float(input('Insira um segundo valor: '))
+
+soma = num1 + num2
+print(f"{soma:.{2}f}")
